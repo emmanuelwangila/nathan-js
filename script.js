36a606ed870c979b42ad098a2a3dc74215aa345a
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-03-26 07:34:41 +0300

    flattening an array effectively

--- script.js
+++ script.js
@@ -33,3 +33,13 @@ document.querySelector("#schools").addEventListener("click", (event) => {
     window.location.href += "#" + event.target.id;
   }
 });
+
+let arrays = [
+  [1, 2],
+  [3, 4],
+  [4, 5, 6, 7],
+  [8, 9, [10, 11], 12, 14],
+];
+
+const flatArr = arrays.flat(2);
+console.log(flatArr);
