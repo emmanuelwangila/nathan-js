dbe32defe87978c569780ec3e24198d76c3edc52
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-03-26 06:50:40 +0300

    interview prep

---
+++ index.js
@@ -0,0 +1,11 @@
+const array = [3, 4, 7, 8, 89];
+
+const mappedArray = array.map((arr) => {
+  return arr + 2;
+});
+
+const forArray = array.forEach((arr, i) => {
+  return (array[i] = arr + 3);
+});
+
+console.log(mappedArray, forArray, array);
