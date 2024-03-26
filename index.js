cf1da5be70f11f518a98b37d6214a8bedb542a61
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-03-26 06:58:47 +0300

    understand well null undefined

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ console.log(mappedArray, forArray, array);
 
 console.log(typeof null);
 console.log(typeof undefined);
+
+let a = null;
+console.log(a);
+
+//  double == compare's without matching types -coercian
+
+console.log(null == undefined);
+//  tripple equals compares types and values...
+console.log(null === undefined);
